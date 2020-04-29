e71dac85a3a6d21b56d93ce6fcf96fab5e6551ae
tuyendatma <62637890+tuyendatma@users.noreply.github.com>
2020-04-29 14:30:43 +0700

    29/04/2020

---
+++ src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        double a;
+        do {
+            System.out.println("nhap gia tri a khac 0:");
+            a = scanner.nextDouble();
+        }while (a==0);
+        System.out.println("nhap gia tri b");
+        double b = scanner.nextDouble();
+        System.out.println("nhap gia tri c");
+        double c =scanner.nextDouble();
+        QuadraticEquation quadraticEquation = new QuadraticEquation(a,b,c);
+        double delta =quadraticEquation.getDiscriminant();
+        if(delta<0){
+            System.out.println("phuong trinh vo nghiem!");
+        }else if(delta==0){
+            System.out.println("phuong trinh co nghiem kep: "+quadraticEquation.getRootDouble());
+        }else {
+            System.out.println("phuong trinh co 2 nghiem la: "+quadraticEquation.getRoot1()+
+                    " va "+quadraticEquation.getRoot2());
+        }
+    }
+}
